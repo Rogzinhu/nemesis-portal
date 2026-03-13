7ff95a32b9c3172de65e3fa29303f0583b116eab
Rogzinhu <rogkazu@gmail.com>
2026-03-13 19:00:07 -0300

    Change base path in Vite config to root

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Isso força o site a procurar os arquivos na pasta certa
-  build: {
-    outDir: 'dist',
-  }
+  base: '/', 
 })
